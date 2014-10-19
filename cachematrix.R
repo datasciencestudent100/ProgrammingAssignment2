fc6ee671f2504dd99c2541bc6c469e26c2aeaa4b
datasciencestudent100 <jameschen382@gmail.com>
2014-10-18 23:34:21 -0500

    Commit Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+# In this assignment we use the <<- operator to assign a matrix to an object in an 
+# environment that is different from the current environment. Below are two functions 
+# that are used to create a special object that stores a matrix and cache the inverse matrix.
+# 
+# The first function, makeCacheMatrix creates a special "matrix", which is really a list 
+# containing a function to
+# 
+#     set the matrix 
+#     get the matrix
+#     set the inverse matrix
+#     get the inverse matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(inputMatrix = numeric()) {
+    inverseMatrix <- NULL 
+    set <- function(y) {
+        inputMatrix <<- y      # set the inputMatrix with y input value
+        inverseMatrix <<- NULL  # set the default NULL to the inverse Matrix
+    }
+    
+    get <- function() inputMatrix  # get the Matrix
+    
+    setinverse <- function(inverseM) inverseMatrix <<- inverseM   # set the inverse Matrix
+    getinverse <- function() inverseMatrix   # get the inverse Matrix
+    
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+# The following function calculates the inverse matrix with solve() function. However, 
+# it first checks to see if the inverse matrix has already been calculated.
+# If so, it gets the inverse matrix from the cache and skips the computation. Otherwise, 
+# it calculates the inverse matrix and sets the value of the inverse matrix in the cache via 
+# the setinvers function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    if(!is.null(x$getinvers())) {  # check inverse matrix is not null
+        message("getting cached inverse Matrix")
+        return(x$getinvers())
+    }
+    else {         # if inverse matrix is null
+        matrix <- x$get()  
+        inverseMatrix <- solve(matrix)  # calculate inverse marix
+        x$setinverse(inverseMatrix)     # set the inverse matrix for the object
+        inverseMatrix                   # retrun inverse matrix
+    }
+    
 }
